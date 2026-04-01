b92bb9b7caae73f98c12aa8d7d3bdeca6ffb4bb3
daniellongford <daniel.longford1@gmail.com>
2026-04-01 20:15:53 +1000

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,8 +4,3 @@ const supabaseUrl = 'https://nbwgavnygpemeloznuqo.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5id2dhdm55Z3BlbWVsb3pudXFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwMTc1ODQsImV4cCI6MjA5MDU5MzU4NH0.toJ9Z-_2t2csJEJ7QTp7ljcI6WFumONAUB3HoRi9zDc'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
-```
-
-**Commit message:**
-```
-Add Supabase connection
