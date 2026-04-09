83ede9b48e1152c511bd1adb9b26211dfd2c58fb
daniellongford <daniel.longford1@gmail.com>
2026-04-09 17:38:58 +1000

    move supabase keys to environment variables

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://nbwgavnygpemeloznuqo.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5id2dhdm55Z3BlbWVsb3pudXFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwMTc1ODQsImV4cCI6MjA5MDU5MzU4NH0.toJ9Z-_2t2csJEJ7QTp7ljcI6WFumONAUB3HoRi9zDc'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
