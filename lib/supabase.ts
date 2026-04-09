6a1cebdde9d3b13408f0df93611b758dbb0a3520
daniellongford <daniel.longford1@gmail.com>
2026-04-09 17:48:46 +1000

    revert supabase to hardcoded keys

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = 'https://nbwgavnygpemeloznuqo.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5id2dhdm55Z3BlbWVsb3pudXFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwMTc3NTg0LCJleHAiOjIwOTA1OTM1ODR9.toJ9Z-_2t2csJEJ7QTp7ljcI6WFumONAUB3HoRi9zDc'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
