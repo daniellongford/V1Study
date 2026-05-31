f294a9ca05f16865c80c83b5a6935a3cdb571489
daniellongford <daniel.longford1@gmail.com>
2026-05-31 22:10:38 +1000

    Update route.ts

--- app/api/support-email/route.ts
+++ app/api/support-email/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: NextRequest) {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer re_jorGiwah_7GJgo154iW3fTYmETsvXWaGK`,
+        'Authorization': `Bearer ${process.env.RESEND_API_KEY}`,
       },
       body: JSON.stringify({
         from: 'V1 Study <support@v1study.com.au>',
